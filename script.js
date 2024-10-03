47a53d460b883d937626d8e717004905a8cea1f2
saransk03 <saransk753@gmail.com>
2024-10-04 02:46:41 +0530

    Add product and about

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 // nav items event
 
 const navItems = document.querySelectorAll(".nav-items");
+const gradientItems = document.querySelectorAll(".gradient");
+const cardItems = document.querySelectorAll(".card")
 
 
 
@@ -12,4 +14,30 @@ navItems.forEach(items =>{
     items.addEventListener("mouseout",() =>{
         items.style.borderBottom = "";
     });
+})
+
+
+cardItems.forEach(car =>{
+
+    const gradientItems = car.querySelectorAll(".gradient");
+
+    car.addEventListener("mouseover",() =>{
+        gradientItems.forEach(item =>{
+        item.style.opacity ="1"
+        }) 
+        car.style.transform = "scale(1.07)";
+        car.style.transition = "all 0.3s ease";
+        car.style.boxShadow = "0px 0px 20px rgba(254, 195, 0, 0.9)";
+})
+    car.addEventListener("mouseout",() =>{
+        gradientItems.forEach(item =>{
+            item.style.opacity ="0"
+        }) 
+        car.style.transform = "scale(1)";
+        car.style.transition = "all 0.3s ease";
+        car.style.boxShadow = "0px 0px 0px rgba(254, 195, 0, 0.0)";
+
+
+
+})
 })
